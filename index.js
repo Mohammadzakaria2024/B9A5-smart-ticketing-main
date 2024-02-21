5baf589b0f6ca7ba8f271eed726d24783470852a
Mohammad Zakaria <mdzakaria3010@gmail.com.com>
2024-02-22 03:50:27 +0600

    finish dessign

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-var namr = 5000;
-console.log(namr);
+function liftseatq(id){
+  const liftseat = document.getElementsByClassName(id).innerText;
+  const convertliftseat = parseInt(liftseat);
+  return convertliftseat;
+
+}
+
+const result = liftseatq("vaton-name");
+console.log(result);
+  
+
+const allbutton = document.getElementsByClassName("Baton-name");
+
+console.log(allbutton);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
