f0f63cf2ce78e49388f514eb72c81900731a11d2
Mohammad Zakaria <mdzakaria3010@gmail.com.com>
2024-02-23 04:11:02 +0600

    tecut counter

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-function liftseatq(id){
-  const liftseat = document.getElementsByClassName(id).innerText;
-  const convertliftseat = parseInt(liftseat);
-  return convertliftseat;
+const allbtn = document.querySelectorAll(".Baton-name");
 
+const rice = 550;
+const economoy = "Economoy";
+
+for (const btn of allbtn) {
+  btn.addEventListener("click", function (event) {
+    const setname = event.target.innerText;
+
+    const seletediv = document.querySelector('.div'); // Assuming you have an element with class 'div'
+    seletediv.classList.add("flex");
+    const div1 = document.createElement("div");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+
+    p1.innerText = setname;
+    p2.innerText = economoy;
+    p3.innerText = rice;
+
+    div1.appendChild(p1);
+    div1.appendChild(p2);
+    div1.appendChild(p3);
+
+    seletediv.appendChild(div1);
+  });
 }
-
-const result = liftseatq("vaton-name");
-console.log(result);
-  
-
-const allbutton = document.getElementsByClassName("Baton-name");
-
-console.log(allbutton);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
